e7a667a270c3e4564bdae6b940edb4f9d5e50061
gabrielharo-dotcom <gabriel.haro@tecsup.edu.pe>
2026-05-13 14:29:26 -0500

    My first component

--- cinespoilers/src/App.tsx
+++ cinespoilers/src/App.tsx
@@ -1,8 +1,8 @@
+import MyProfile from "./components/MyProfile"
+
 function App() {
   return (
-    <div className="App">
-      <h1>Gabriel Haro</h1>
-    </div>
+    <MyProfile />
   )
 }
 
---
+++ cinespoilers/src/components/MyProfile.tsx
@@ -0,0 +1,10 @@
+const MyProfile = () => {
+    return ( 
+        <header>
+            <h1>Gabriel Haro</h1>
+            <h2>Frontend Developer</h2>
+        </header>
+     );
+}
+ 
+export default MyProfile;
